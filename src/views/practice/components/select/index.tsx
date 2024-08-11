5b1d8b99b3765e51dee9347c633dc1969fb4a7da
duhuanwen <duhuanwen@163.com>
2024-08-11 18:02:57 +0800

    2024.8.11

--- src/views/practice/components/select/index.tsx
+++ src/views/practice/components/select/index.tsx
@@ -1,3 +1,8 @@
 import Select from "./select";
+import { SelectRef } from "./select";
+
+export * from "./interface";
+
+export type { SelectRef };
 
 export default Select;
--- src/views/test/index.tsx
+++ src/views/test/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Select from "../practice/components/select";
-import { selectData } from "./data";
-// const crypto = require("crypto");
 
 const Test: React.FC = () => {
-  // console.log(crypto.randomBytes(18), "111");
-
   return (
     <>
-      <Select selectData={selectData} isRadio={true} />
+      <div></div>
+      <br />
     </>
   );
 };
