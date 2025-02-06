2aa74a8d299228f6e546f1547871a771f31e90e0
xuheng <xuheng@im.nrxt.com>
2025-02-06 15:53:16 +0800

    111111111

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Test from "./views/test";
-import Detail from "./views/test/prictice/time/components/detail";
-// import { useState } from "react";
-// import { Button } from "antd";
-// import "./App.css";
-// import { IntlProvider } from "react-intl";
-// import { enUS } from "./views/locale/en-US.ts";
-// import { zhCN } from "./views/locale/zh-CN.ts";
-
-// const messages: Record<string, any> = {
-//   "en-US": enUS,
-//   "zh-CN": zhCN,
-// };
 
 function App() {
-  // const [locale, setLocale] = useState("zh-CN");
   return (
     <>
       <HashRouter>
         <Routes>
           <Route path="/" element={<Test />} />
           <Route path="/test" element={<Test />} />
-          <Route path="/detail" element={<Detail />}></Route>
         </Routes>
       </HashRouter>
-      {/* </IntlProvider> */}
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { IntlProvider } from "react-intl";
 import "./index.css";
 import { ClickToComponent } from "click-to-react-component";
-import { enUS } from "./views/locale/en-US.ts";
-import { zhCN } from "./views/locale/zh-CN.ts";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-const messages: Record<string, any> = {
-  "en-US": enUS,
-  "zh-CN": zhCN,
-};
-const locale = "zh-CN";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <DndProvider backend={HTML5Backend}>
-    <IntlProvider messages={messages[locale]} locale={locale}>
-      <ClickToComponent />
-      <App />
-    </IntlProvider>
+    <ClickToComponent />
+    <App />
   </DndProvider>
 );
