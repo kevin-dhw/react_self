171b3e7e8dfb278247c48ec85cb314b84a557dcc
kevin <932731452@qq.com>
2025-06-20 14:04:26 +0800

    init stock

---
+++ src/views/selfApp/stock/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Stock: React.FC = () => {
+  return (
+    <div className=" h-[100vh]">
+      stcok
+      {/* <AdventiseAnalysis></AdventiseAnalysis> */}
+      {/* <Btns></Btns> */}
+    </div>
+  );
+};
+
+export default Stock;
--- src/views/test/index.tsx
+++ src/views/test/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 // import StandardCard from "../selfApp/workSearch";
 // import Btns from "../selfApp/btns";
-import AdventiseAnalysis from "../pc/adventiseAnalysis";
+// import AdventiseAnalysis from "../pc/adventiseAnalysis";
+import Stock from "../selfApp/stock";
 
 const Test: React.FC = () => {
   return (
     <div className=" h-[100vh]">
       {/* <StandardCard></StandardCard> */}
-      <AdventiseAnalysis></AdventiseAnalysis>
+      {/* <AdventiseAnalysis></AdventiseAnalysis> */}
       {/* <Btns></Btns> */}
+      <Stock></Stock>
     </div>
   );
 };
